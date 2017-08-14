b54d24d5b1b6a7bc2f2bbde8d13ee6245fc96a11
RomuloCintra <romulo.cintra.contractor@bbva.com>
2017-08-14 14:04:27 +0200

    feat(baseConfig) seting bases config

--- src/baseConfig/baseConfig.ts
+++ src/baseConfig/baseConfig.ts
@@ -1,2 +1,12 @@
+import { checkNode } from '../utils/versions';
 import { IBase } from './base';
 
+export function baseConfig(config: IBase) {
+  // Check Node Version
+  if (config['node-version']) {
+    checkNode(config['node-version'], config.name);
+  }
+
+  console.log(config['logo-ascii']);
+  console.log(config['welcome-message']);
+}
